30687d536f2c67055d6f95c92cb3ee778fc908d7
Henrik N <henrik@nyh.se>
2013-06-24 19:16:03 +0200

    Receive payload from GitHub

--- app.rb
+++ app.rb
@@ -5,3 +5,8 @@ Bundler.require :default, (ENV['RACK_ENV'] || "development").to_sym
 get "/" do
   "Hello at #{Time.now}"
 end
+
+post "/" do
+  push = JSON.parse(params[:payload])
+  logger.debug "I got some JSON: #{push.inspect}"
+end
