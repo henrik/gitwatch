b31fb600ab2f7f47dc8fd895fa671d094c441780
Henrik N <henrik@nyh.se>
2013-06-24 20:14:04 +0200

    Some example triggers.

--- app.rb
+++ app.rb
@@ -19,10 +19,26 @@ post "/" do
 
   commits.each do |commit|
     touched_paths = commit["modified"] | commit["removed"] | commit["added"]
+    commit_url = commit["url"]
 
-    logger.info "URL: #{commit["url"].inspect}"
+    logger.info "URL: #{commit_url.inspect}"
     logger.info "Paths: #{touched_paths.inspect}"
+
+    paths = touched_paths.select { |path| path.include?(".css") }
+    if paths.any?
+      logger.info "!! CSS gatekeeper: #{commit_url} touched #{paths.inspect}"
+    end
+
+    paths = touched_paths.select { |path| path.include?("spec_helper") || path.include?("spec/support") }
+    if paths.any?
+      logger.info "!! Spec setup nerd: #{commit_url} touched #{paths.inspect}"
+    end
+
+    paths = touched_paths.select { |path| path.include?("bovary") }
+    if paths.any?
+      logger.info "!! Bovarian: #{commit_url} touched #{paths.inspect}"
+    end
   end
 
-  "hi"
+  "OK!"
 end
